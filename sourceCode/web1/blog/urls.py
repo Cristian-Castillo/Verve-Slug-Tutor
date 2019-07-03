a3aa730b2f8cc6d384ddc71e42b6784baa89e254
dayna-k <dkim197@ucsc.edu>
2019-07-03 14:07:56 -0700

    edit url link

--- sourceCode/web1/blog/urls.py
+++ sourceCode/web1/blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
-    path('signup/', views.signup, name='blog-login'),
+    path('login/', views.login, name='blog-login'),
     path('signup/', views.signup, name='blog-signup'),
 ]
