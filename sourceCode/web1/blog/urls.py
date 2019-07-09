5e3b5a9172e6e8b0e1eac0f1990126e9b7109d0d
Cristian-Castillo <cristiancarrillocastillo@gmail.com>
2019-07-09 13:20:31 -0700

    Authentication Functionality: Firebase API-Login
    Firebase connected to login.html webpage. User must manually enter their email in Firebase which will enable them the credentials to login and access the knowledge board page. The login features now have functionality!

--- sourceCode/web1/blog/urls.py
+++ sourceCode/web1/blog/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('', views.home, name='blog-home'),
@@ -7,4 +10,9 @@ urlpatterns = [
     path('login/', views.login, name='blog-login'),
     path('signup/', views.signup, name='blog-signup'),
     path('knowledge/', views.knowledge, name='blog-knowledge'),
+    path('postsign/',views.postsign, name='blog-knowledge'), #CC: added postsign path so when user signs up will redirect you to knowledge board
 ]
+
+
+# CC note: you will not be able to access the knowledge board page unless you login. You may add
+#      yourself manually from Verve-Slug-Tutor firebase console under the authentication tab to gain entrance!
--- sourceCode/web1/blog/views.py
+++ sourceCode/web1/blog/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+import pyrebase # CC: imported pyrebase wrapper, essentially derives from firebase July 9, 2019
+from django.contrib import auth
+
+# CC: Included API from Verve-Slug-Tutor and succesfully integrated authentication ------------------------------------------
+config = {
+    'apiKey': "AIzaSyBHHg2e5gRop8tcO2RReu8paiEXJbGomVw",
+    'authDomain': "verve-slug-tutor.firebaseapp.com",
+    'databaseURL': "https://verve-slug-tutor.firebaseio.com",
+    'projectId': "verve-slug-tutor",
+    'storageBucket': "verve-slug-tutor.appspot.com",
+    'messagingSenderId': "1013220996189",
+    'appId': "1:1013220996189:web:462bd852caeea191"
+  }
+
+firebase = pyrebase.initialize_app(config) # enables the pyrebase wrapper to be used, passed API into function
+authe = firebase.auth()
+
+#CC:  End of API ----------------------------------------------------------------------------------------------------------------
 
 posts = [
     {
@@ -25,11 +43,30 @@ def home(request):
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
 
-def login(request):
-    return render(request, 'blog/login.html', {'title': 'Login'})
-
 def signup(request):
     return render(request, 'blog/signup.html', {'title': 'Signup'})
 
 def knowledge(request):
     return render(request, 'blog/knowledge.html', {'title': 'Knowledge'})
+
+def login(request):
+    return render(request, "blog/login.html", {'title': 'Login'})
+
+# CC:added postsign which gets the email, and if information is entered correctly it will send you to -----------------------------
+def postsign(request): 
+    email=request.POST.get('email')
+    passw = request.POST.get("pass")
+    try:
+        user = authe.sign_in_with_email_and_password(email,passw)
+    except:
+        message = "invalid credentials"
+        return render(request,"login.html",{"messg":message})
+
+    print(user['idToken']) 
+    session_id=user['idToken']
+    request.session['uid']=str(session_id)
+    return render(request, "blog/knowledge.html",{"e":email})
+# CC: from print down --------------------------------------------------------------------------------------------------------------
+
+
+
