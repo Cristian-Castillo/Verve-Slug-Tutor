111d9f11938c0526fadd8727e3e8e71b3abc45f4
Cristian-Castillo <cristiancarrillocastillo@gmail.com>
2019-07-14 12:15:49 -0700

    Add files via upload

--- sourceCode/web1/blog/views.py
+++ sourceCode/web1/blog/views.py
@@ -45,63 +45,99 @@ def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
 
 def signup(request):
-    return render(request, 'blog/signup.html', {'title': 'Signup'})
-
-def knowledge(request):
-    return render(request, 'blog/knowledge.html', {'title': 'Knowledge'})
-
-def login(request):
-    return render(request, "blog/login.html", {'title': 'Login'})
-
-def contact(request):
-    return render(request, "blog/contact.html", {'title': 'Contact'})
-
-def profile(request):
-    return render(request, 'blog/profile.html', {'title': 'Profile'})
-
-# CC:added postsign which gets the email, and if information is entered correctly it will send you to -----------------------------
-def postsign(request):
-    email=request.POST.get('email')
-    passw = request.POST.get("pass")
-    try:
-        user = authe.sign_in_with_email_and_password(email,passw)
-    except:
-        message = "invalid credentials"
-        return render(request,"login.html",{"messg":message})
-
-    print(user['idToken'])
-    session_id=user['idToken']
-    request.session['uid']=str(session_id)
-    return render(request, "blog/knowledge.html",{"e":email})
-
-def postsignup(request):
-
+   # Sun 11:23
     name=request.POST.get('name')
     name2=request.POST.get('name2')
     email=request.POST.get('email')
     contact=request.POST.get('contact')
     passw=request.POST.get('pass')
 
-
-    # data={
-    #     "ID":id,
-    #     "PW":pw,
-    #     "FN":fname.lower(),
-    #     "LN":lname.lower(),
-    #     "EMAIL":email,
-    #     "CONTACT":contact
-    # }
-
     user=authe.create_user_with_email_and_password(email,passw)
+    session_id=user['idToken']
     # CC: Here we create account
-
     uid = user['localId']
     # CC unique ID for the user
 
-    data = {"name":name, "lastname":name2, "email":email, "contact":contact, "status":"1"}
+    data = {"name":name, "lastname":name2, "email":email, "contact":contact, "id":uid}
     # to push the data into the database, 1 means account is enabled
     # from above name and email from form and enabled the account
     # database constructor with multiple users
-    database.child("users").child(uid).child("details").set(data)
-    return render(request,"knowledge.html")
+    database.child("users").child(uid).child("details").set(data,session_id)
+    return render(request,"blog/signup.html")
+
+def login(request):
+    email=request.POST.get('email')
+    passw = request.POST.get("pass")
+    try: 
+        user = authe.sign_in_with_email_and_password(email,passw)
+    except:
+        message = "invalid credentials"
+        return render(request,"blog/login.html",{"messg":message})
+
+    print(user['idToken'])
+    session_id=user['idToken']
+    request.session['uid']=str(session_id)
+    return render(request, "blog/knowledge.html",{"e":email})
+
+def contact(request):
+    return render(request, "contact.html", {'title': {'Contact'}})
+
+def knowledge(request):
+    idtoken = request.session['uid']
+    a = authe.get_account_info(idtoken)
+    a = a['users']
+    a = a[0]
+    a = a['localId']
+    print("ID Token: "+ str(a))
+    try:
+        database.child('users').child(a).child('details').child('id').set(a,idtoken)
+        name = database.child('users').child(a).child('details').child('id').get(idtoken).val()
+        return render(request, 'blog/knowledge.html', {'e':name})
+    except KeyError:
+        message="Opps! User Logged Out!"
+        return render(request,"login.html",{"messg":message})
+
+def profile(request):
+    idtoken = request.session['uid']
+    a = authe.get_account_info(idtoken)
+    a = a['users']
+    a = a[0]
+    a = a['localId']
+    print("ID Token: "+ str(a))
+    try:
+        database.child('users').child(a).child('details').child('id').set(a,idtoken)
+        #name = database.child('users').child(a).child('details').child('id').get().val()
+        name = database.child('users').child(a).child('details').child('id').get(idtoken).val()
+        return render(request, 'blog/profile.html', {"e":name})
+    except KeyError:
+        message =  "Opps! User Logged Out!"
+        return render(request,"login.html",{"messg":message})
+
+def logout(request):
+    idtoken = request.session['uid']
+    a = authe.get_account_info(idtoken)
+    a = a['users']
+    a = a[0]
+    a = a['localId']
+    try:
+        del a
+    except KeyError:
+        pass
+    return render(request,'blog/login.html')
+
+#def check(request):
+ #   idtoken = request.session['uid']
+  #  a = authe.get_account_info(idtoken)
+   # a = a['users']
+   # a = a[0]
+   # a = a['localId']
+   # information = database.child('users').child(a).('details').child('contact').child('email').child('id').child('lastname').child('name').shallow().get().val()
+   # listTime=[] #Help to get all the data
+    #for i in information:
+    #    listTime.append(i)
+
+    #listTime.sort(reverse=True)
+    #print(information)
+    #return render(request,'check.html')
+
 # CC: from print down --------------------------------------------------------------------------------------------------------------
