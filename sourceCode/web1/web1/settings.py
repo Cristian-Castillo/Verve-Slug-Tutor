b361cc52c1b6aae2eb3f1d6ee4748c0770e43691
jrony149 <52219222+jrony149@users.noreply.github.com>
2019-07-16 09:59:21 -0700

    Session persistence
    current page link highlighting, session persistence, user logged in/ user logged out nav bar link hiding

--- sourceCode/web1/web1/settings.py
+++ sourceCode/web1/web1/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'django.contrib.admin',
+    'crispy_forms',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
