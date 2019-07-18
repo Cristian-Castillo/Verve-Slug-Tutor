1cfb8b982c38a1d5fde0130355afd8088c79b4b9
Kim Kisoo <k014520@kaist.ac.kr>
2019-07-18 01:04:17 -0700

    still working

--- sourceCode/web1/blog/urls.py
+++ sourceCode/web1/blog/urls.py
@@ -7,14 +7,19 @@ from django.urls import path, include
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
-    path('login/', views.login, name='blog-login'),
+    path('login/', views.login, name='blog-login'),  #the href links in the html pages are using the "names" here to link themselves with these url patterns which are of course calling functions from the Views.py file
     path('knowledge/', views.knowledge, name='blog-knowledge'),
+    path('knowledge_content/',views.knowledge_content, name='blog-knowledge_content'),
+    path('knowledge_content/update',views.knowledge_content_update, name='blog-knowledge_content_update'),
     path('signup/',views.signup, name='blog-signup'),
+    path('postsignup/',views.postsignup, name='blog-postsignup'),
     path('contact/',views.contact, name='blog-contact'),
     path('profile/',views.profile, name='blog-profile'),
-    path('logout/',views.logout, name='blog-logout'),
-    path('knowledge_content/', views.knowledge_content, name='blog-knowledge_content')
+    path('postsign/', views.postsign),
+    path('post_form/', views.post_form, name='blog-post_form'),
+    path('logout/', views.logout, name="blog-logout")
 ]
+
 
 
 # CC note: you will not be able to access the knowledge board page unless you login. You may add
--- sourceCode/web1/blog/views.py
+++ sourceCode/web1/blog/views.py
@@ -19,33 +19,146 @@ database= firebase.database() # created the database
 
 #CC:  End of API ----------------------------------------------------------------------------------------------------------------
 
-posts = [
-    {
-        'author': 'Brian Alegria',
-        'title': 'Tutoring Post 1',
-        'content': 'I need tutoring in advanced physica 8A and I am excellent web dev',
-        'date_posted': 'June 30th, 2019'
-    },
-    {
-        'author': 'Another User',
-        'title': 'Tutoring Post 2',
-        'content': 'Second Post Content',
-        'date_posted': 'June 30th, 2019'
-    }
 
-]
 
 def home(request):
-    context = {
-    'posts': posts
-    }
-    return render(request, 'blog/home.html', context)
+
+    message = "hide"
+    try:
+        idtoken = request.session['uid']
+
+        return render(request, "blog/home.html", {"title": "Profile"})
+
+    except KeyError:
+
+        return render(request, "blog/home.html", {"messg": message})
+
+
 
 def about(request):
-    return render(request, 'blog/about.html', {'title': 'About'})
+
+    message = "hide"
+    try:
+        idtoken = request.session['uid']
+
+        return render(request, "blog/about.html", {"title": "About"})
+
+    except KeyError:
+
+        return render(request, "blog/about.html", {"messg": message})
+
 
 def signup(request):
-   # Sun 11:23
+
+    return render(request, 'blog/signup.html', {'title': 'Signup'})
+
+
+def post_form(request):
+    message = "Please log in to access this feature."
+
+    try:
+        idtoken = request.session['uid']
+        return render(request, 'blog/post_form.html', {'title': 'Post'})
+
+
+    except KeyError:
+
+        return render(request, "blog/login.html", {"messg": message})
+
+
+def knowledge(request):
+    message = "Please log in to access this feature."
+
+    try:
+        idtoken = request.session['uid']
+        localID = authe.get_account_info(idtoken)['users'][0]['localId']
+        name = database.child('users').child(localID).child('details').child('name').get().val()
+        return render(request, "blog/knowledge.html", {"e": name})
+
+    except KeyError:
+
+        return render(request, "blog/login.html", {"messg": message})
+
+def knowledge_content(request):
+    message = "Please log in to access this feature."
+
+    try:
+        idtoken = request.session['uid']
+        localID = authe.get_account_info(idtoken)['users'][0]['localId']
+        name = database.child('users').child(localID).child('details').child('name').get().val()
+        return render(request, "blog/knowledge_content.html", {"e": name})
+
+    except KeyError:
+
+        return render(request, "blog/login.html", {"messg": message})
+
+def knowledge_content_update(request):
+    message = "Please log in to access this feature."
+
+    try:
+        idtoken = request.session['uid']
+        localID = authe.get_account_info(idtoken)['users'][0]['localId']
+        name = database.child('users').child(localID).child('details').child('name').get().val()
+        return render(request, "blog/knowledge_content_update.html", {"e": name})
+
+    except KeyError:
+
+        return render(request, "blog/login.html", {"messg": message})
+
+
+def login(request):
+    return render(request, "blog/login.html", {'title': 'Login'})
+
+def contact(request):
+
+    message = "hide"
+    try:
+        idtoken = request.session['uid']
+
+        return render(request, "blog/contact.html", {"title": "Contact"})
+
+    except KeyError:
+
+        return render(request, "blog/contact.html", {"messg": message})
+
+
+def profile(request):
+    message = "Please log in to access this feature."
+    try:
+        idtoken = request.session['uid']
+
+        return render(request, "blog/profile.html", {"title": "Profile"})
+
+
+    except KeyError:
+
+        return render(request, "blog/login.html", {"messg": message})
+
+# CC:added postsign which gets the email, and if information is entered correctly it will send you to -----------------------------
+def postsign(request): #Changes made by JR in order to display name instead of email
+    email=request.POST.get('email')
+    passw = request.POST.get('pass')
+
+    print("email")
+    print("passw")
+    try:
+        user = authe.sign_in_with_email_and_password(email,passw)
+    except:
+        message = "invalid credentials"
+        return render(request,"login.html",{"messg":message})
+
+    #print(user['idToken'])
+    session_id=user['idToken']
+    request.session['uid']=str(session_id)
+    idtoken = request.session['uid']
+    #a = authe.get_account_info
+
+    localID = authe.get_account_info(idtoken)['users'][0]['localId']
+    name = database.child('users').child(localID).child('details').child('name').get().val()
+    return render(request, "blog/knowledge.html",{"e": name})
+
+def postsignup(request):
+
     name=request.POST.get('name')
     name2=request.POST.get('name2')
     email=request.POST.get('email')
@@ -53,106 +166,23 @@ def signup(request):
     passw=request.POST.get('pass')
 
     user=authe.create_user_with_email_and_password(email,passw)
-    session_id=user['idToken']
     # CC: Here we create account
+
     uid = user['localId']
     # CC unique ID for the user
 
-    data = {"name":name, "lastname":name2, "email":email, "contact":contact, "id":uid}
+    data = {"name":name, "lastname":name2, "email":email, "contact":contact, "status":"1"}
     # to push the data into the database, 1 means account is enabled
     # from above name and email from form and enabled the account
     # database constructor with multiple users
-    database.child("users").child(uid).child("details").set(data,session_id)
-    return render(request,"blog/signup.html")
-
-def login(request):
-    email=request.POST.get('email')
-    passw = request.POST.get("pass")
+    database.child("users").child(uid).child("details").set(data)
+    return render(request,"knowledge.html")
+# CC: from print down --------------------------------------------------------------------------------------------------------------
+def logout(request): #JR deletes session; if there is no session to delete, render login page regardless
     try:
-        user = authe.sign_in_with_email_and_password(email,passw)
-    except:
-        message = "invalid credentials"
-        return render(request,"blog/login.html",{"messg":message})
+        del request.session['uid']
 
-    print(user['idToken'])
-    session_id=user['idToken']
-    request.session['uid']=str(session_id)
-    return render(request, "blog/knowledge.html",{"e":email})
-
-def contact(request):
-    return render(request, "contact.html", {'title': {'Contact'}})
-
-def knowledge(request):
-    idtoken = request.session['uid']
-    a = authe.get_account_info(idtoken)
-    a = a['users']
-    a = a[0]
-    a = a['localId']
-    print("ID Token: "+ str(a))
-    try:
-        database.child('users').child(a).child('details').child('id').set(a,idtoken)
-        name = database.child('users').child(a).child('details').child('id').get(idtoken).val()
-        return render(request, 'blog/knowledge.html', {'e':name})
-    except KeyError:
-        message="Opps! User Logged Out!"
-        return render(request,"login.html",{"messg":message})
-
-def profile(request):
-    idtoken = request.session['uid']
-    a = authe.get_account_info(idtoken)
-    a = a['users']
-    a = a[0]
-    a = a['localId']
-    print("ID Token: "+ str(a))
-    try:
-        database.child('users').child(a).child('details').child('id').set(a,idtoken)
-        #name = database.child('users').child(a).child('details').child('id').get().val()
-        name = database.child('users').child(a).child('details').child('id').get(idtoken).val()
-        return render(request, 'blog/profile.html', {"e":name})
-    except KeyError:
-        message =  "Opps! User Logged Out!"
-        return render(request,"login.html",{"messg":message})
-
-def logout(request):
-    idtoken = request.session['uid']
-    a = authe.get_account_info(idtoken)
-    a = a['users']
-    a = a[0]
-    a = a['localId']
-    try:
-        del a
     except KeyError:
         pass
-    return render(request,'blog/login.html')
 
-def knowledge_content(request):
-    idtoken = request.session['uid']
-    a = authe.get_account_info(idtoken)
-    a = a['users']
-    a = a[0]
-    a = a['localId']
-    print("ID Token: "+ str(a))
-    try:
-        database.child('users').child(a).child('details').child('id').set(a,idtoken)
-        name = database.child('users').child(a).child('details').child('id').get(idtoken).val()
-        return render(request, 'knowledge_content.html', {'e':name})
-    except KeyError:
-        message="Opps! User Logged Out!"
-        return render(request,"login.html",{"messg":message})
-
-#def check(request):
- #   idtoken = request.session['uid']
-  #  a = authe.get_account_info(idtoken)
-   # a = a['users']
-   # a = a[0]
-   # a = a['localId']
-   # information = database.child('users').child(a).('details').child('contact').child('email').child('id').child('lastname').child('name').shallow().get().val()
-   # listTime=[] #Help to get all the data
-    #for i in information:
-    #    listTime.append(i)
-
-    #listTime.sort(reverse=True)
-    #print(information)
-    #return render(request,'check.html')
-
-# CC: from print down --------------------------------------------------------------------------------------------------------------
+    return render(request, 'login.html')
